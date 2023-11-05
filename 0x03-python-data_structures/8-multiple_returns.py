3d0e5db10e3525669d54a8f7aef4807ab97a2c84
Arsany199 <arsanykamal30@gmail.com>
2023-11-05 08:30:59 -0800

    8-multiple_returns.py

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -4,7 +4,7 @@ def multiple_returns(sentence):
     length = len(sentence)
     first = sentence[0]
     if len(sentence) == 0:
-        return first is None
+        return first == 0, "None"
     else:
         my_tuple = length, first
         return my_tuple
