28ed45d27104bc151b6698e99f35bd4b86cbd6c2
Arsany199 <arsanykamal30@gmail.com>
2023-11-05 08:16:51 -0800

    8-multiple_returns.py

---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def multiple_returns(sentence):
+    length = len(sentence)
+    first = sentence[0]
+
+    if sentence == "":
+        return first == None
+    else:
+        return length, first
