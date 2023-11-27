71d7e37cb90b1e2ad301171683af332a0c285381
Arsany199 <arsanykamal30@gmail.com>
2023-11-27 04:35:22 -0800

    1-rectangle.py

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -21,7 +21,7 @@ class Rectangle:
         """function that sets the width of rectangle"""
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
-        
+
         if value < 0:
             raise ValueError("width must be >= 0")
 
