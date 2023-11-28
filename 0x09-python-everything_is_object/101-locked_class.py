174e8c3359ebd52fe2ce7159d1e4b0cb681953f8
Arsany199 <arsanykamal30@gmail.com>
2023-11-28 10:37:17 -0800

    101-locked_class.py

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
+"""Define a function lockedClass"""
 
 
 class LockedClass:
+    """function that prevent from creating new attribute except first_name"""
     __slots__ = ["first_name"]
