b993154019bc3d09583f84e6b1aad02b16292f08
Arsany199 <arsanykamal30@gmail.com>
2023-12-04 05:36:40 -0800

    1-my_list.py

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Defines the class MyList"""
+
+
+class MyList(list):
+    """class mylist inheritance of the class list"""
+    pass
+
+    def print_sorted(self):
+        """function that prints the list sorted"""
+
+        print(sorted(self))
