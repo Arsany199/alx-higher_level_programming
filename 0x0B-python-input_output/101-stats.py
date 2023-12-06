f9b049bdcb15f1ef19d8c79ac5363d97381ea073
Arsany199 <arsanykamal30@gmail.com>
2023-12-06 03:32:06 -0800

    101-stats.py

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -10,6 +10,8 @@ def print_stats(size, status_codes):
     for key in sorted(status_codes):
         print("{}: {}".format(key, status_codes[key]))
 
+if __name__ = "__main__":
+
     size = 0
     status_codes = {}
     v_c = ['200', '301', '400', '401', '403', '404', '405', '500']
@@ -44,6 +46,3 @@ def print_stats(size, status_codes):
     except KeyboardInterrupt:
         print_stats(size, status_codes)
         raise
-
-if __name__ == "__main__":
-    print_status()
