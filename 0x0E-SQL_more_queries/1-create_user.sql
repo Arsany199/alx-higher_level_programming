d432d8ae057aae47ad229ef56217128a165d1ca9
Arsany199 <arsanykamal30@gmail.com>
2024-01-17 07:24:57 -0800

    1-create_user.sql

---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,4 @@
+-- create a user and then give him all the privileges
+CREATE USER `user_0d_1` IDENTIFIED BY `user_0d_1_pwd`;
+GRANT ALL PRIVILEGES ON *.* TO `user_0d_1`@`localhost`;
+FLUSH PRIVILEGES;
