d5d92a7b035d9767137808353ccb939d669b9e11
Arsany199 <arsanykamal30@gmail.com>
2024-01-17 07:14:52 -0800

    0-privileges.sql

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,3 +1,3 @@
 -- give privileges to users in the localhost
-GRANTS FOR 'user_0d_1'@'localhost';
-GRANTS FOR 'user_0d_2'@'localhost';
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
