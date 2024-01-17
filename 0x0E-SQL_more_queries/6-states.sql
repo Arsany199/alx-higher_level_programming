540ed319498fcd9caafdad6d4ca4a60ae4e963a8
Arsany199 <arsanykamal30@gmail.com>
2024-01-17 08:08:42 -0800

    6-states.sql

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,5 @@
+-- create database usa and table states with it attributes
+CREATE DATABASE IF NOT EXISTS `hbtn_0d_usa`;
+CREATE TABLE IF NOT EXISTS `hbtn_0d_usa`.`states`(
+`id` INT UNIQUE AUTO_INCREMENT NOT NULL PRIMARY KEY, 
+`name` VARCHAR(256) NOT NULL);
