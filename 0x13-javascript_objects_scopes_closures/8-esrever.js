eb97d59e1a90fc21136fe4a33a59295051614bf6
Arsany199 <arsanykamal30@gmail.com>
2024-02-13 09:34:23 -0800

    8-esrever.js

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,9 +1,8 @@
 #!/usr/bin/node
 // reverse the list
 exports.esrever = function (list) {
-  let myList = [];
-  for (let index = list.length - 1; index >= 0; index--) {
-    myList.push(list[index]);
-  }
-  return myList;
+  return list.reduceRight(function (array, current) {
+    array.push(current);
+    return array;
+  }, []);
 };
