332b9cebf9f0e19370f354b18209e863b18c665d
Arsany199 <arsanykamal30@gmail.com>
2024-02-13 08:48:57 -0800

    2-rectangle.js

---
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+// defines rectangle with width and height if postive
+class Rectangle {
+  constructor (w, h) {
+    if (w <= 0 || h <= 0 || w === undefined || h === undefined) {
+      return this;
+    } else {
+      this.width = w;
+      this.height = h;
+    }
+  }
+}
+module.exports = Rectangle;
