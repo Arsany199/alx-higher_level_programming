3a6009e94214adffd5b4a80a476d291abe591545
Arsany199 <arsanykamal30@gmail.com>
2024-02-13 09:13:05 -0800

    5-square.js

---
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+// class square that inherits the rectangle
+module.exports = class Square extends require('./4-rectangle.js') {
+  constructor (size) {
+    super(size, size);
+  }
+};
