b942c9675877073e7fc2f7de8f1ab577f5c9103e
arsany199 <arsanykamal30@gmail.com>
2024-02-29 09:27:57 -0800

    6-peak.py

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,10 +1,21 @@
 #!/usr/bin/python3
-"""find the peak in a list of ints"""
+"""Module that finds a peak in a list of integers"""
+
 
 def find_peak(list_of_integers):
-    my_max = None
-    my_list = list_of_integers
-    for i in my_list:
-        if my_max is None or my_max < i:
-            my_max = i
-    return my_max
+    """function that finds a peak in a list of unsorted integers"""
+    mylist = list_of_integers
+    if mylist == []:
+        return None
+    length = len(mylist)
+
+    start, end = 0, length - 1
+    while start < end:
+        mid = start + (end - start) // 2
+        if mylist[mid] > mylist[mid - 1] and mylist[mid] > mylist[mid + 1]:
+            return mylist[mid]
+        if mylist[mid - 1] > mylist[mid + 1]:
+            end = mid
+        else:
+            start = mid + 1
+    return mylist[start]
