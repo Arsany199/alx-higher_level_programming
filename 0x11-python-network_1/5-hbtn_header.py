76a5105ec95a113be98e54967d8d29e83f1289a4
arsany199 <arsanykamal30@gmail.com>
2024-03-01 13:10:17 -0800

    5-hbtn_header.py

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 """return header from the url"""
-from sys import argv
+import sys
 import requests
 
-if __name__=="__main__":
+if __name__ == "__main__":
     res = requests.get(sys.argv[1])
     print(res.headers.get("X-Request-Id"))
