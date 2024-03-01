9ac6cbb45fc9bfab98abd2848a76ef18bd221865
arsany199 <arsanykamal30@gmail.com>
2024-03-01 11:27:07 -0800

    4-hbtn_status.py

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -7,7 +7,7 @@ url = 'https://intranet.hbtn.io/status'
 
 
 if __name__ == '__main__':
-    requ = requests.get(url)
+    req = requests.get(url)
     print("Body response:")
-    print("\t- type: {}".format(type(requ.text)))
-    print("\t- content: {}".format(requ.text))
+    print("\t- type: {}".format(type(req.text)))
+    print("\t- content: {}".format(req.text))
