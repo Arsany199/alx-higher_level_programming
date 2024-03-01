856c9c181b789b10637edc6152ead312c73c7e96
arsany199 <arsanykamal30@gmail.com>
2024-03-01 13:25:48 -0800

    7-error_code.py

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -4,9 +4,8 @@ followed by the value of the HTTP status code"""
 import requests
 import sys
 
-url = sys.argv[1]
-
 if __name__ == "__main__":
+    url = sys.argv[1]
     myresp = requests.get(url)
     if myresp.status_code >= 400:
         print("Error code: {}".format(myresp.status_code))
