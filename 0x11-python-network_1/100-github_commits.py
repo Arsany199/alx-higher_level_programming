34f4c738acccd67a1be9e028491c69f26ee51050
arsany199 <arsanykamal30@gmail.com>
2024-03-01 14:02:38 -0800

    100-github_commits.py

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -5,8 +5,9 @@ import sys
 
 if __name__ == "__main__":
     url = "https://api.github.com/repos/{}/{}/commits".format(argv[2], argv[1])
-    r = requests.get(url)
-    commits = r.json()
+    req = requests.get(url)
+    commits = req.json()
+
     try:
         for ind in range(10):
             print("{}: {}".format(
