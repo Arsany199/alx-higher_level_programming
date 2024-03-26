4dc7e35fbcfe78584f0f74beaf14d969311cd665
arsany199 <arsanykamal30@gmail.com>
2024-03-26 09:50:33 -0700

    4-starwars_count.js

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,22 +1,21 @@
 #!/usr/bin/node
-
 const request = require('request');
 const url = process.argv[2];
 
-request(url, (err, data, body) => {
+request(url, function (err, data, body) {
   if (err) {
     console.log(err);
   } else {
-    let x = 0;
+    let counter = 0;
     const films = JSON.parse(body).results;
-    for (let i = 0; i < films.length; i++) {
-      const characters = films[i].characters;
+    for (let result = 0; result < films.length; result++) {
+      const characters = films[result].characters;
       for (let j = 0; j < characters.length; j++) {
         if (characters[j] === 'https://swapi-api.hbtn.io/api/people/18/' || characters[j] === 'http://swapi-api.hbtn.io/api/people/18/') {
-          x += 1;
+          counter += 1;
         }
       }
     }
-    console.log(x);
+    console.log(counter);
   }
 });
