45a2c89f6404b0fddbe984ad9a96f4bf7a8befd6
arsany199 <arsanykamal30@gmail.com>
2024-03-26 09:00:00 -0700

    2-statuscode.js

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -3,7 +3,7 @@
 const request = require('request');
 const myurl = process.argv[2];
 
-request(myurl, (err, response) => {
+request(myurl, (err, response, body) => {
   if (err) {
     console.log(err);
   } else {
