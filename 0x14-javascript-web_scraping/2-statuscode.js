c1b8d55bb1655e98f7032c7a187f928a8c4bed82
arsany199 <arsanykamal30@gmail.com>
2024-03-26 08:58:24 -0700

    2-statuscode.js

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+const request = require('request');
+const myurl = process.argv[2];
+
+request(myurl, (err, response) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log('code: ', response.statusCode);
+  }
+});
